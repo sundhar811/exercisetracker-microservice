6e1e5f448ef362494ed2fbb5e74914b48c2c3769
Glitch (sundhar811-exercisetracker-microservice) <none>
2019-10-07 11:23:19 +0000

    🐠🐩 Updated with Glitch

--- server.js
+++ server.js
@@ -1,11 +1,42 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-
+const shortid = require('shortid');
 const cors = require('cors')
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MLAB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+let userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  }, 
+  _id: {
+    type: String
+  }
+})
+
+let User = mongoose.model('user', userSchema);
+
+let exerciseSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    required: true
+  }, 
+  description: {
+    type: String,
+    required: true
+  },
+  duration: {
+    type: Number,
+    required: true
+  },
+  date: Date
+})
+
+let Exercise = mongoose.model('exercise', exerciseSchema);
 
 app.use(cors())
 
@@ -18,6 +49,64 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+
+// user creation
+app.post('/api/exercise/new-user', (req, res) => {
+  let { username } = req.body;
+  let user = new User({
+    username: username,
+    _id: shortid.generate()
+  });
+  
+  user.save((err, data) => {
+    if(err) res.send(err);
+    else res.send(data);
+  });
+})
+
+// exercise add
+app.post('/api/exercise/add', (req, res) => {
+  let { userId, description, date, duration } = req.body;
+  User.findOne({ _id: userId }, (err, data) => {
+    if(err) console.log(err)
+    else {
+      if (data) {
+        if (description && duration) {
+          let exercise = new Exercise({
+            userId: userId,
+            description: description,
+            duration: duration,
+            date: date ? new Date(date) : new Date()
+          })
+          exercise.save((err, data) => {
+            if (err) res.send(err)
+            else res.send(data)
+          })
+        }
+      } else {
+        res.send({error: 'The user does not exist'})
+      }
+    }
+  })
+})
+
+// get all users
+app.get('/api/exercise/users', (req, res) => {
+  User.find({}, (err, data) => {
+    if(err) res.send(err)
+    else res.send(data)
+  })
+})
+
+// get exercise log
+app.get('/api/exercise/log?userId=:userId', (req, res) => {
+  let { userId, from, to, limit } = req.params;
+  console.log(req.query, req.params)
+  Exercise.find({ userId: userId }, (err, data) => {
+    if(err) res.send(err)
+    else res.send(data)
+  })
+})
 
 // Not found middleware
 app.use((req, res, next) => {
